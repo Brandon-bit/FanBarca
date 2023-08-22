79647e6a741eeb372d2ebb6572e9e25eb3004b9b
Brandon Juarez <brandonsalinas792@gmail.com>
2023-08-22 10:52:04 -0600

    starting the project

---
+++ src/FanBarca/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace FanBarca.Models.Domain;
+
+public class DatabaseContext : IdentityDbContext<ApplicationUser>
+{
+    
+}
---
+++ src/FanBarca/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FanBarca.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
